a385d7964027c25eec1fabb2950cc57015dba0be
cristian gianoboli <cristian.gianoboli@artekium.com>
2025-12-19 15:55:18 -0300

    validation schema, Joi package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,22 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 
 @Module({
   imports: [
     //Siempre declararlo al inicio
     ConfigModule.forRoot({
-      load: [ EnvConfiguration ]
+      load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema
     }),
 
     ServeStaticModule.forRoot({
